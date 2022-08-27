ded3788df1233f38fc13f6495ff82d1f32f3d7c7
Liuchang <1264057941@qq.com>
2022-08-27 23:39:41 +0800

    second

---
+++ src/demo/HelloWorld.java
@@ -0,0 +1,14 @@
+package demo;
+
+/**
+ * @author liuchang
+ * @Create 2022-08-27-23:09
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("你好啊啊啊啊啊啊啊啊!!");
+        System.out.println("啊啊啊");
+        System.out.println("你好啊啊啊啊啊啊啊啊!!");
+        System.out.println("啊啊啊");
+    }
+}
